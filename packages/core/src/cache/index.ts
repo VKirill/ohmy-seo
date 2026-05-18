fc49ad33904efd7725d2de58637221f164ade056
VKirill <vechkasov@gmail.com>
2026-05-18 14:09:13 +0300

    Phase 1.4b: add barrel re-exports for subpath imports

---
+++ packages/core/src/cache/index.ts
@@ -0,0 +1,4 @@
+export * from './cache-keys.js';
+export * from './cache-policy.js';
+export * from './cache-stats.js';
+export * from './query-cache-repo.js';
---
+++ packages/core/src/crypto/index.ts
@@ -0,0 +1,2 @@
+export * from './master-key.js';
+export * from './secret-cipher.js';
